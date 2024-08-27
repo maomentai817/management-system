4b7e42e1a00683f5110a7193202805ed1b9e67a1
mmt817 <1669404371@qq.com>
2024-08-27 20:31:03 +0800

    card组件

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,8 +1,8 @@
 // 将 components 文件夹下的所有组件进行全局化注册
-import TempComponent from './modules/TempComponent/TempComponent.vue'
+import CardContainer from "./modules/CardContainer/CardContainer.vue"
 
 export const componentPlugin = {
   install(app) {
-    app.component('TempComponent', TempComponent)
+    app.component('CardContainer', CardContainer)
   }
 }
---
+++ frontend/src/components/modules/CardContainer/CardContainer.vue
@@ -0,0 +1,47 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <el-card class="page-container">
+    <template #header v-if="title">
+      <div class="header f-b">
+        <b>{{ title }}</b>
+        <div class="extra f-s">
+          <slot name="extra"></slot>
+        </div>
+      </div>
+    </template>
+    <div class="content h-full fd-col">
+      <slot></slot>
+    </div>
+  </el-card>
+</template>
+
+<style lang="scss" scoped>
+.page-container {
+  box-sizing: border-box;
+
+  :deep(.el-card__header) {
+    padding-top: 8px;
+    padding-bottom: 8px;
+  }
+
+  :deep(.el-card__body) {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    overflow: auto;
+    height: 100%;
+  }
+
+  .content {
+    max-height: 100%;
+    overflow-y: auto;
+  }
+}
+</style>
